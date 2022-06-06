c24d9f7ca703563ac589b474465e3a98e64a69d2
ojizero <z.acnts.816@pm.me>
2022-06-06 23:56:36 +0300

    added missing static analysis tools

---
+++ tools/tools.go
@@ -0,0 +1,7 @@
+//go:build tools
+
+// Package tools is a stub for development tooling to be tracked by Gomod
+package tools
+
+// Static analysis tooling
+import _ "honnef.co/go/tools/cmd/staticcheck"
